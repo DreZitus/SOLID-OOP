1efb1e6ef99961dcd939c98094b4d980297fc1ff
André <108902451+DreZitus@users.noreply.github.com>
2025-11-08 14:31:51 +0000

    refatorando classes com SRP

--- PHP-OOP/solid_principles.php
+++ PHP-OOP/solid_principles.php
@@ -1,19 +1,28 @@
 <?php
 
-class Order{
+class Ordem
+{
     public function calculateTotalSum(){/*...*/}
     public function getItems(){/*...*/}
     public function getItemCount(){/*...*/}
     public function addItem($item){/*...*/}
     public function deleteItem($item){/*...*/}
     
-    public function printOrder(){/*...*/}
-    public function showOrder(){/*...*/}
-    
+}
+
+class OrdemRepositorio
+{
     public function load(){/*...*/}
     public function save(){/*...*/}
     public function update(){/*...*/}
     public function delete(){/*...*/}
 }
+
+class Ordemvisivel
+{
+    public function printOrder(){/*...*/}
+    public function showOrder(){/*...*/}
+}
+
 ?>
 
