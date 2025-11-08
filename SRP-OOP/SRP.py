0c1698015fd673bc012a3e1999b778798976c77b
André <108902451+DreZitus@users.noreply.github.com>
2025-11-08 14:38:28 +0000

    criando classes SRP em python

---
+++ SRP-OOP/SRP.py
@@ -0,0 +1,16 @@
+class ordem(self):
+    def calculaSomaTotal()
+    def pegaDados()
+    def pegaContagemDados()
+    def addDados()
+    def deletaDados()
+
+class OrdemRepositorio(self)
+    def carregamento()
+    def salva()
+    def Update()
+    def delete()
+
+class ordemVisualizacao(self)
+    def printOrdem()
+    def mostraOrdem()
